af943993c29ac1b2a10b81b7a58798aa4fde62b9
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-30 17:51:38 +0530

    Add model class

---
+++ BSEB_QR_Encrypt_Service/Models/QREncryptedData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSEB_QR_Encrypt_Service.Models
+{
+    public class QREncryptedData
+    {
+        public string RollCode { get; set; }
+        public string RollNo { get; set; }
+        public string EncryptedData { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
