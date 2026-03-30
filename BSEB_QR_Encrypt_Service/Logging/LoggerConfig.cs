6fabe0b2f5e4e25dc67ce0dbaf2231a72103d361
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-30 18:03:23 +0530

    Add loger

---
+++ BSEB_QR_Encrypt_Service/Logging/LoggerConfig.cs
@@ -0,0 +1,9 @@
+﻿using Serilog;
+
+public static class LoggerConfig
+{
+    public static void Configure()
+    {
+        Log.Logger = new LoggerConfiguration().WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+    }
+}
