55f424ccd7110fb1235af561f259664508e68b6e
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-30 17:51:22 +0530

    Add model class

---
+++ BSEB_QR_Encrypt_Service/Models/SubjectResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSEB_QR_Encrypt_Service.Models
+{
+    public class SubjectResult
+    {
+        public string? Sub { get; set; }
+        public int? MaxMark { get; set; }
+        public int? PassMark { get; set; }
+        public string? Theory { get; set; }
+        public string? OB_PR { get; set; }
+        public string? GRC_THO { get; set; }
+        public string? GRC_PR { get; set; }
+        public string? TotSub { get; set; }
+        public string? SubjectGroupName { get; set; }
+        public string? CCEMarks { get; set; }
+    }
+}
