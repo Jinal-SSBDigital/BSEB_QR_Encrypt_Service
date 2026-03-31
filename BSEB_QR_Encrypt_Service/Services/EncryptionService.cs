76b0f72b40b108bf5a2f93515ea8f6f97d428862
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-31 17:19:04 +0530

    add encrypt code

---
+++ BSEB_QR_Encrypt_Service/Services/EncryptionService.cs
@@ -0,0 +1,10 @@
+﻿using BSEB_QR_Encrypt_Service.Models;
+using BSEB_QR_Encrypt_Service.Utilities;
+
+public class EncryptionService
+{
+    public string EncryptStudent(StudentResult student)
+    {
+        return QrUtility.GenerateEncryptedPayloadFull(student);
+    }
+}
