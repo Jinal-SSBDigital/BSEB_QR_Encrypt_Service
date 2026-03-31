458428c3f3083228e3e5fd49548432b9116de09f
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-31 17:16:47 +0530

    add services

---
+++ BSEB_QR_Encrypt_Service/Program.cs
@@ -0,0 +1,28 @@
+﻿using BSEBExamResult_QRGenerate.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddWindowsService();
+
+// Logger
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
+
+// DB
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbcs")));
+
+// DI
+builder.Services.AddScoped<DbHelper>();
+builder.Services.AddScoped<EncryptionService>();
+builder.Services.AddScoped<ProcessingService>();
+
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
