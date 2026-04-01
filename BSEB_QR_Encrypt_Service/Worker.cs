4e8b67b36eff6bcc2abc65b14fddd69a5645b407
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-04-01 16:23:36 +0530

    add services

---
+++ BSEB_QR_Encrypt_Service/Worker.cs
@@ -0,0 +1,21 @@
+﻿public class Worker : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(IServiceProvider serviceProvider, ILogger<Worker> logger)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Service Started");
+
+        using var scope = _serviceProvider.CreateScope();
+        var processor = scope.ServiceProvider.GetRequiredService<ProcessingService>();
+
+        await processor.ProcessAsync(stoppingToken);
+    }
+}
